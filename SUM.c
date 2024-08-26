028e1706e40453928343b775899b5bb38cc274bb
krishnabaheti28 <krishnabaheti2024@gmail.com>
2024-08-26 12:10:34 +0530

    Added new file

---
+++ SUM.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Enter two numbers \n");
+    int a,b,sum;
+    scanf("%d%d", &a, &b);
+    sum = a + b;
+    printf("Sum is %d \n",sum);
+    return 0;
+}
